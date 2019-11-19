31a405a8b63ede17723f5887623a4caaac90fb4d
deuelteogz-i <deuelteogz-i@SG01NB-7800439.snshub.org>
2019-11-19 11:51:24 +0800

    Second commit

--- src/main/java/com/demo/DemoautobuildApplication.java
+++ src/main/java/com/demo/DemoautobuildApplication.java
@@ -11,7 +11,7 @@ public class DemoautobuildApplication {
 	
 	@RequestMapping("/")
 	public String home(){
-		return "Hello world";
+		return "Hello wold";
 	}
 
 	public static void main(String[] args) {
